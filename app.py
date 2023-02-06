ecf10ff8317345a21881295276970f370ba296b4
Curtis Irvine <curtisjirvine@hotmail.com>
2023-02-06 18:14:08 +0000

    basic api working

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 import requests
 import json
 import config
-
-app = Flask(__name__)
 
 # Inputs
 city_name = config.city
@@ -41,5 +39,23 @@ def get_weather(
     return data
 
 
-if __name__== "__main__":
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
     app.run(debug=True)
