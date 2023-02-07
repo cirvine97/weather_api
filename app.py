931b678114e3908466c2b6f70f2666fe502ccf23
Curtis Irvine <curtisjirvine@hotmail.com>
2023-02-07 14:52:49 +0000

    Logging.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, request, render_template
 import requests
 import json
 import config
+import logging
 
 
 def get_lat_lon(
@@ -21,7 +22,7 @@ def get_weather(
     city_name:str,
     API_key:str
 ) -> dict:
-    
+    '''Returns the JSON output of a city's weather.'''
     # Get the lat lon of the city 
     lat_lon = get_lat_lon(city_name=city_name,
                            API_key=API_key
@@ -40,6 +41,7 @@ def surfaced_values(
     API_key:str,
     temperature_units:str
 ) -> dict:
+    '''Format the JSON output to be surfaced on the API'''
     weather_json = get_weather(city_name, API_key)
     description = weather_json['weather'][0]['description']
     icon = weather_json['weather'][0]['icon']
@@ -65,20 +67,28 @@ def surfaced_values(
 
 app = Flask(__name__)
 
+# Start logging
+logging.basicConfig(filename='record.log',
+                    level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
 @app.route("/", methods=["POST", "GET"])
 def input_city():
+    app.logger.error('Error log information')
     if request.method == "POST":
         city = request.form["nm"]
         return redirect(url_for("weather_report", city_name=city))
     else:
+        app.logger.info("Info log information")
         return render_template("input.html")
 
 @app.route("/weather/<city_name>")
 def weather_report(city_name):
+    app.logger.error("Error log information")
     values = surfaced_values(city_name,
                              config.API_key,
                              config.temperature_unit)
     return values
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
